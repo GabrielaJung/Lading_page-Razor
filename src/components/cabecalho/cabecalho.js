e71b702bb56b581e626174fb99106f0d147a0fdf
Gabi Jung <gabrielamjung@gmail.com>
2022-10-17 14:50:52 -0300

    lading-page

---
+++ src/components/cabecalho/cabecalho.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+class Cabecalho extends Component {
+
+
+    render() { 
+        return (
+            <nav className='navbar navbar-default navbar-fixed-top'>
+                <div className='container'>
+                    <div className='navbar-header'>
+
+                    </div>
+                </div>
+
+            </nav>
+        );
+    }
+}
+ 
+export default Cabecalho;
