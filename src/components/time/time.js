da973c273bf69562cc371a09afd745347ebc4280
Gabi Jung <gabrielamjung@gmail.com>
2022-10-30 16:13:45 -0300

    ajuste: card Micklos

--- src/components/time/time.js
+++ src/components/time/time.js
@@ -4,6 +4,7 @@ import './style.css';
 import Gabriela from './img/gabriela.jpg'
 import Yacana from './img/yacana.jpg'
 import Rui from './img/rui.jpg'
+import Micklos from './img/micklos.jpg'
 
 const Time = () => {
     return (
@@ -14,7 +15,7 @@ const Time = () => {
                         <h2 className='d-flex justify-content-center m-4 p-2'>Conheça a nossa equipe NerdScript</h2>     
                     </div>
 
-                    <div className='col-lg-4 col-md-6 col-sm-12 container d-flex justify-content-center'>
+                    <div className='col-lg-3 col-md-6 col-sm-12 container d-flex justify-content-center'>
                         <div className="card m-2 mb-5">
                             <img src={Gabriela} className="card-img-top" alt="Gabriela" />
                             <div className="card-body">
@@ -25,7 +26,7 @@ const Time = () => {
                             </div>
                         </div>                        
                     </div>
-                    <div className='col-lg-4 col-md-6 col-sm-12 container d-flex justify-content-center'>
+                    <div className='col-lg-3 col-md-6 col-sm-12 container d-flex justify-content-center'>
                         <div className="card m-2 mb-5">
                             <img src={Yacana} className="card-img-top" alt="Yaçanã" />
                             <div className="card-body">
@@ -36,14 +37,25 @@ const Time = () => {
                             </div>
                         </div>                        
                     </div>
-                    <div className='col-lg-4 col-md-6 col-sm-12 container d-flex justify-content-center'>
+                    <div className='col-lg-3 col-md-6 col-sm-12 container d-flex justify-content-center'>
                         <div className="card m-2 mb-5">
                             <img src={Rui} className="card-img-top" alt="Rui" />
                             <div className="card-body">
                                 <h5 className="card-title d-flex justify-content-center">Rui Vicente</h5>
                                 <p className="card-text d-flex justify-content-center">Aluno do #TeuFuturo 2022</p>
                                 <a href="mailto: rui.vicente2004@gmail.com subject=Olá!" className='links d-flex justify-content-center'>E-mail</a>
-                                <a href="https://www.linkedin.com/in/gabriela-jung-53b345227/" className='links d-flex justify-content-center'>LinkedIn</a>
+                                <a href="https://www.linkedin.com/in/" className='links d-flex justify-content-center'>LinkedIn</a>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className='col-lg-3 col-md-6 col-sm-12 container d-flex justify-content-center'>
+                        <div className="card m-2 mb-5">
+                            <img src={Micklos} className="card-img-top" alt="Micklos" />
+                            <div className="card-body">
+                                <h5 className="card-title d-flex justify-content-center">Micklos Rodrigues</h5>
+                                <p className="card-text d-flex justify-content-center">Aluno do #TeuFuturo 2022</p>
+                                <a href="mailto: micklosrodrigues10@gmail.com subject=Olá!" className='links d-flex justify-content-center'>E-mail</a>
+                                <a href="https://www.linkedin.com/in/" className='links d-flex justify-content-center'>LinkedIn</a>
                             </div>
                         </div>                        
                     </div>
